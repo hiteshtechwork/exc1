321fc79efcb9bd8d95aca37820f8439cc46c740a
hvrishabh <hitesh.verma@rishabhsoft.com>
2024-05-28 19:09:39 +0530

    2nd commit

---
+++ web/modules/custom/my_custom_api/src/Plugin/rest/resource/NodeResource copy.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace Drupal\my_custom_api\Plugin\rest\resource;
+
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\rest\Plugin\ResourceBase;
+use Drupal\rest\ResourceResponse;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Provides a REST API endpoint for nodes.
+ *
+ * @RestResource(
+ * id = "node_resource",
+ * label = @Translation("Node Resource"),
+ * uri_paths = {
+ * "canonical" = "/api/nodes"
+ * }
+ * )
+ */
+class NodeResource extends ResourceBase
+{
+
+/**
+ * The entity type manager service.
+ *
+ * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+ */
+    protected $entityTypeManager;
+
+/**
+ * Constructs a new NodeResource object.
+ *
+ * @param array $configuration
+ * A configuration array containing information about the plugin instance.
+ * @param string $plugin_id
+ * The plugin_id for the plugin instance.
+ * @param mixed $plugin_definition
+ * The plugin implementation definition.
+ * @param array $serializer_formats
+ * The available serializer formats.
+ * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+ * The entity type manager service.
+ */
+    public function __construct(array $configuration, $plugin_id, $plugin_definition, array $serializer_formats,
+        EntityTypeManagerInterface $entity_type_manager) {
+        parent::__construct($configuration, $plugin_id, $plugin_definition, $serializer_formats);
+        $this->entityTypeManager = $entity_type_manager;
+    }
+
+/**
+ * {@inheritdoc}
+ */
+    public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition)
+    {
+        return new static(
+            $configuration,
+            $plugin_id,
+            $plugin_definition,
+            $container->getParameter('serializer.formats'),
+            $container->get('entity_type.manager')
+        );
+    }
+
+/**
+ * Responds to GET requests.
+ *
+ * @return \Drupal\rest\ResourceResponse
+ * The response containing the node data.
+ */
+    public function get()
+    {
+        $nodes = $this->entityTypeManager->getStorage('node')->loadMultiple();
+        $data = [];
+
+        foreach ($nodes as $node) {
+            $data[] = [
+                'id' => $node->id(),
+                'title' => $node->label(),
+// Add other fields as needed
+            ];
+        }
+
+        return new ResourceResponse($data);
+    }
+}
